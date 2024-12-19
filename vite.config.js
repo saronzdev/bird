177a20a22ae69877cf09f94b571ec7517e80f2d3
saronzdev <ronaldsardinas05@gmail.com>
2024-12-19 14:37:29 -0500

    new features

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,24 @@
 import {defineConfig} from 'vite'
 import {config} from 'dotenv'
 import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
 config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+  build: {
+      outDir: 'dist',
+      assetsDir: 'assets',
+      rollupOptions: {
+        output: {
+          chunkFileNames: 'assets/js/[name]-[hash].js',
+          entryFileNames: 'assets/js/[name]-[hash].js',
+          assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
+        }
+      }
+    },
   define: {
     'process.env': process.env
   }
