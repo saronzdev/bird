4a84ee92b04061430702f3fc24af7f4727e2ea20
saronzdev <ronaldsardinas05@gmail.com>
2024-12-19 15:33:02 -0500

    new features

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import {Route, BrowserRouter, createBrowserRouter, RouterProvider, Routes} from 'react-router-dom'
 import Home from './pages/Home.jsx'
 import Login from './pages/Login.jsx'
 import User from './pages/User.jsx'
@@ -6,35 +6,18 @@ import Welcome from './pages/Welcome.jsx'
 import NotFound from './pages/NotFound.jsx'
 import Register from './pages/Register.jsx'
 
-const router = createBrowserRouter([
-	{
-		path: '/',
-		element: <Welcome/>,
-    errorElement: <NotFound/>
-	},
-  {
-    path: '/home',
-    element: <Home/>,
-  },
-  {
-    path: '/signup',
-    element: <Register/>
-  },
-  {
-    path: '/signin',
-    element: <Login/>
-  },
-	{
-		path: '/user/:username',
-		element: <User/>,
-	}
-])
-
 function App() {
 	return (
-		<>
-			<RouterProvider router={router} />
-		</>
+		<BrowserRouter>
+			<Routes>
+        <Route path='/' element={<Welcome/>} />
+        <Route path='/home' element={<Home/>} />
+        <Route path='/signin' element={<Login/>} />
+        <Route path='/signup' element={<Register/>} />
+        <Route path='/suer/:username' element={<User/>} />
+        <Route path='*' element={<NotFound/>} />
+      </Routes>
+		</BrowserRouter>
 	)
 }
 
