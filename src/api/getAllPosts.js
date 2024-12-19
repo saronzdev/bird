423eb2dcca6c8dec6f8e125bd13c629a8d61fa6b
saronzdev <ronaldsardinas05@gmail.com>
2024-12-19 13:46:05 -0500

    new features

--- src/api/getAllPosts.js
+++ src/api/getAllPosts.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import shuffle from 'shuffle-array'
 
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL && 'https://bird-api-alpha.vercel.app/'
 
 export const getAllPosts = async (setData, setLoading) => {
 	try {
